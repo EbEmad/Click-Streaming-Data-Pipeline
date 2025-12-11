a00b72c124df6f981211c4c61a94344e96ab78a2
Ebrahim Emad <ebemad8@gmail.com>
2025-12-11 11:10:02 +0200

    update

--- services/document/app/pg/database.py
+++ services/document/app/pg/database.py
@@ -2,12 +2,12 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
 
-from ..config import get_settings
+from .config import get_settings
 
 settings = get_settings()
 
 engine = create_async_engine(
-    settings.database_url,
+    settings.database_url.replace("postgresql://", "postgresql+asyncpg://"),
     echo=False,
     pool_size=20,
     max_overflow=40,
